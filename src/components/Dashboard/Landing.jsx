1698379da653ff85e486383adb9c81cd187499ae
Faith-vaati <vaati.faith79@gmail.com>
2024-08-05 12:09:04 +0300

    grid CSS for the top sect of the landing page and the rest grid defination for the grid rows

--- src/components/Dashboard/Landing.jsx
+++ src/components/Dashboard/Landing.jsx
@@ -1,7 +1,32 @@
+import "../../Styles/Landing.scss";
+import "../../Styles/_grid.scss";
 export default function Landing() {
-    return (
-      <div>
-        <h1>Landing Page</h1>
+  return (
+    <div className="landing">
+      <div className="landing-top">
+        <div className="top-row">
+          <div className="col-1-of-4">
+            <div className="top-sect-1">1</div>
+            <div className="top-sect-2">2</div>
+            <div className="top-sect-3">3</div>
+          </div>
+          <div className="col-1-of-4">
+            <div className="top-sect-1">1</div>
+            <div className="top-sect-2">2</div>
+            <div className="top-sect-3">3</div>
+          </div>
+          <div className="col-1-of-4">
+            <div className="top-sect-1">1</div>
+            <div className="top-sect-2">2</div>
+            <div className="top-sect-3">3</div>
+          </div>
+          <div className="col-1-of-4">
+            <div className="top-sect-1">1</div>
+            <div className="top-sect-2">2</div>
+            <div className="top-sect-3">3</div>
+          </div>
+        </div>
       </div>
-    );
-    }
+    </div>
+  );
+}
