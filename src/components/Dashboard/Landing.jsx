65f20edcf1cda22d9dd210b5a6c1ccd7ae527375
Faith-vaati <vaati.faith79@gmail.com>
2024-08-05 16:12:43 +0300

    Landing Page top section

--- src/components/Dashboard/Landing.jsx
+++ src/components/Dashboard/Landing.jsx
@@ -1,32 +1,130 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../../Styles/Landing.scss";
 import "../../Styles/_grid.scss";
+import {
+  faArrowUp,
+  faBell,
+  faChartLine,
+  faExclamation,
+  faGem,
+  faList,
+  faMoneyBillTrendUp,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 export default function Landing() {
   return (
     <div className="landing">
-      <div className="landing-top">
         <div className="top-row">
           <div className="col-1-of-4">
-            <div className="top-sect-1">1</div>
-            <div className="top-sect-2">2</div>
-            <div className="top-sect-3">3</div>
+            <div className="top-sect">
+              <div className="top-sect_1">
+                <div className="top-sect_1-icon">
+                  <FontAwesomeIcon className="user" icon={faUser} />
+                </div>
+                <div className="top-sect_1-name">
+                  <p>Tenants</p>
+                </div>
+              </div>
+              <div className="top-sect_2">
+                <p className="top-sect_2-1">1281</p>
+              </div>
+              <div className="top-sect_3">
+                <div className="top-sect_3-icon">
+                  <FontAwesomeIcon className="arrow" icon={faArrowUp} />
+                </div>
+                <div className="top-sect_3-name">
+                  <p>
+                    <span className="p-color">30 %</span> since Apr 2023
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="col-1-of-4">
-            <div className="top-sect-1">1</div>
-            <div className="top-sect-2">2</div>
-            <div className="top-sect-3">3</div>
+            <div className="top-sect">
+              <div className="top-sect_1">
+                <div className="top-sect_1-icon">
+                  <FontAwesomeIcon className="bill" icon={faMoneyBillTrendUp} />
+                </div>
+                <div className="top-sect_1-name">
+                  <p>Total Rent Collected</p>
+                </div>
+              </div>
+              <div className="top-sect_2">
+                <p className="top-sect_2-2">
+                  <span className="top-sect_2-2-currency">Kes</span> 2.5 M
+                </p>
+              </div>
+              <div className="top-sect_3">
+                <div className="top-sect_3-icon">
+                  <FontAwesomeIcon className="chart" icon={faChartLine} />
+                </div>
+                <div className="top-sect_3-name">
+                  <p>View Reports</p>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="col-1-of-4">
-            <div className="top-sect-1">1</div>
-            <div className="top-sect-2">2</div>
-            <div className="top-sect-3">3</div>
+            <div className="top-sect">
+              <div className="top-sect_1">
+                <div className="top-sect_1-icon">
+                  <FontAwesomeIcon className="excl" icon={faExclamation} />
+                </div>
+                <div className="top-sect_1-name">
+                  <p> Rent Arrears</p>
+                </div>
+              </div>
+              <div className="top-sect_2">
+                <p className="top-sect_2-3">
+                  <span className="top-sect_2-2-currency">Kes</span> 250,000
+                </p>
+              </div>
+              <div className="top-sect_3">
+                <div className="top-sect_3-icon">
+                  <FontAwesomeIcon className="bell" icon={faBell} />
+                </div>
+                <div className="top-sect_3-name">
+                  <p>Send Reminders</p>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="col-1-of-4">
-            <div className="top-sect-1">1</div>
-            <div className="top-sect-2">2</div>
-            <div className="top-sect-3">3</div>
+            <div className="top-sect">
+              <div className="top-sect_1">
+                <div className="top-sect_1-icon">
+                  <FontAwesomeIcon className="gem" icon={faGem} />
+                </div>
+                <div className="top-sect_1-name">
+                  <p>Assets & Inventory</p>
+                </div>
+              </div>
+              <div className="top-sect_2">
+                <p className="top-sect_2-4">1281</p>
+              </div>
+              <div className="last-two">
+                <div className="top-sect_3">
+                  <div className="top-sect_3-icon">
+                    <FontAwesomeIcon className="list" icon={faList} />
+                  </div>
+                  <div className="top-sect_3-name">
+                    <p>View all</p>
+                  </div>
+                </div>
+                <div className="top-sect_3">
+                  <div className="top-sect_3-icon">
+                    <FontAwesomeIcon className="chart" icon={faChartLine} />
+                  </div>
+                  <div className="top-sect_3-name">
+                    <p>See Reports</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
       </div>
-    </div>
+      
+      </div>
   );
 }
